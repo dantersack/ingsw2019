27dfec9a74d96c83c6e5b0a8d00b970029da8868
dante <sack_dante@hotmail.com>
2019-05-30 01:59:00 -0300

    port

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ const app = express();
 const path = require('path');
 
 // settings
-app.set('port', 3000)
+//app.set('port', 3000)
 
 // routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.listen(app.get('port'), () => {
+/*app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
 });
+*/
